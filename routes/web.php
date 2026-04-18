6b9a03cb716c31cdd4f036c5904c0746198bf432
raihann2826 <d.raihan@students.amikom.ac.id>
2026-04-18 11:28:58 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -8,8 +11,8 @@ Route::get('/', function () {
 
 Route::get('/tentang', function () {
 
-    return 'h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
-    });
+return 'h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
+});
 
 Route::get('/contact', function() {
 return view('contact');
@@ -24,4 +27,14 @@ Route::get('/bantuan', function () {
     return view('bantuan');
 });
 
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/event', [EventController::class, 'show'])->name('events.show');
+Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
+Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
+Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
 
+Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
+
+Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
+// dan seterusnya...
+});
