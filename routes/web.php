108054ce120ee3057d9cbd1c4a2a35c6bf2e4a7c
raihann2826 <d.raihan@students.amikom.ac.id>
2026-04-18 11:51:13 +0700

    Tugas 2

--- routes/web.php
+++ routes/web.php
@@ -5,30 +5,8 @@ use App\Http\Controllers\EventController;
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/tentang', function () {
-
-return 'h1>Ini adalah Halaman Tentang Aplikasi Event Hub</h1>';
-});
-
-Route::get('/contact', function() {
-return view('contact');
-});
-Route::get('/profil', function () {
-    return view('profil');
-});
-Route::get('/katalog', function () {
-    return view('katalog');
-});
-Route::get('/bantuan', function () {
-    return view('bantuan');
-});
-
 Route::get('/', [HomeController::class, 'index'])->name('home');
-Route::get('/event', [EventController::class, 'show'])->name('events.show');
+Route::get('/event/1', [EventController::class, 'show'])->name('events.show');
 Route::get('/checkout', [EventController::class, 'checkout'])->name('checkout');
 Route::get('/my-ticket', [EventController::class, 'ticket'])->name('ticket');
 Route::group(['prefix' => 'admin', 'as' => 'admin.'], function () {
@@ -38,3 +16,28 @@ Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
 Route::get('/events', [EventController::class, 'indexAdmin'])->name('events.index');
 // dan seterusnya...
 });
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/tentang', function() {
+    return '<h1>Ini adalah halaman tentang aplikasi Event Hub</h1>';
+});
+
+Route::get('/contact', function() {
+    return view('contact');
+});
+
+Route::get('/profil', function(){
+    return view('profil');
+});
+
+Route::get('/katalog', function(){
+    return view('katalog');
+});
+
+Route::get('/bantuan', function(){
+    return view('bantuan');
+});
+
+// Route::get('/',)
